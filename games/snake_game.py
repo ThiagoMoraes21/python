e421db618b72f8510e76b3ee88ce7dda3ab9963c
ThiagoMoraes21 <thiago4x70@gmail.com>
2024-02-07 00:27:13 -0300

    change game fps rate to 10

--- games/snake_game.py
+++ games/snake_game.py
@@ -88,6 +88,6 @@ while running:
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(10)  # limits FPS to 60
 
 pygame.quit()
